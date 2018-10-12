0bd9cfffaa9fdd127cb44e9f51c0031ffaa88b50
J Nielsen <buhros@hotmail.com>
2018-10-12 09:40:54 +0200

    Update FourInARow-consoleGame/FourInARow.cs

--- FourInARow-consoleGame/FourInARow.cs
+++ FourInARow-consoleGame/FourInARow.cs
@@ -8,29 +8,5 @@ namespace FourInARow_consoleGame
 {
     class FourInARow
     {
-        char[,] _twoDimensionalArray = new char[6,6];
-
-        /// <summary>
-        /// get a singel value from our two dimensional array
-        /// </summary>
-        /// <param name="x">x axis</param>
-        /// <param name="y">y axis</param>
-        /// <returns></returns>
-        public char GetSingleValue(int x, int y)
-        {
-            return _twoDimensionalArray[x, y];
-        }
-
-        /// <summary>
-        /// Set a single value our two dimensional array
-        /// </summary>
-        /// <param name="x">x axis position</param>
-        /// <param name="y">y axis position</param>
-        /// <param name="value">the char you want to set</param>
-        /// <returns></returns>
-        public char SetSingleValue(int x, int y, char value)
-        {
-             return _twoDimensionalArray[x, y] = value;
-        }
     }
 }
