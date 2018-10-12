8978a63c6c486f7f52c5205d2f76df5233ea8ae5
J Nielsen <buhros@hotmail.com>
2018-10-12 13:30:23 +0200

    Turn
    Working turns
    
    No win/end conditions

--- FourInARow-consoleGame/Game.cs
+++ FourInARow-consoleGame/Game.cs
@@ -13,10 +13,36 @@ namespace FourInARow_consoleGame
         BoardGrid _grid = new BoardGrid(); //opretter instans af BoardGrid så det eksisterer i scope (giver adgang til metoder i klassen)
         public Game()
         {
+            bool continueGame = true; //TODO flyt
             SetPlayer(_playerOne);
             SetPlayer(_playerTwo);
             GetGrid();
 
+            while (continueGame)
+            {
+                Turn(_playerOne);
+                Turn(_playerTwo);
+            }
+        }
+
+        /// <summary>
+        /// plays a turn of the game
+        /// </summary>
+        /// <param name="player">Denotes which players turn is it</param>
+        public void Turn(Player player)
+        {
+            int choice = 0;
+            while (choice < 1 || choice > 6) //checker om bruger input er mellem 1 og 6
+            {
+                Console.WriteLine($"{player.Name} choose a column: (1-6)");
+                choice = Convert.ToInt16(Console.ReadLine());
+                if (choice < 1 || choice > 6)
+                {
+                    Console.Write("Invalid, try again: ");
+                }
+            }
+            FindSpot(choice, player); //kalder findspot med brugerens input og den specifikke brugers token
+            GetGrid(); //TODO: win conditions / full column
         }
 
         /// <summary>
@@ -28,11 +54,11 @@ namespace FourInARow_consoleGame
             Console.Write("Please choose a name: ");
             player.Name = Console.ReadLine();
             string input = "";
-            while (input != null && input.Length != 1)
+            while (input == null || input.Length != 1) //checker om imput er tom string eller mere end en character
             {
                 Console.Write("Please choose a token: ");
                 input = Console.ReadLine();
-                if (input != null && input.Length != 1)
+                if (input == null || input.Length != 1)
                 {
                     Console.Write("Invalid, try again: ");
                 }
@@ -45,14 +71,15 @@ namespace FourInARow_consoleGame
         /// checks grid column for empty cell - uses SetSingleValue to change value
         /// </summary>
         /// <param name="col">Column choice</param>
-        public void FindSpot(int col)
+        /// <param name="player">Denotes which players turn it is</param>
+        public void FindSpot(int col, Player player)
         {
             col = col - 1;
             for (int i = _grid.ColLength - 1; i >= 0; i--) //looper kolonne ud fra bruger input - 1 (0 baseret index) nedefra
             {
                 if (_grid.TwoDimensionalArray[i, col].Equals('o')) //hvis værdien i grid er 'o' ændres den til spillerens værdi/karakter
                 {
-                    _grid.SetSingleValue(i, col, 'x');
+                    _grid.SetSingleValue(i, col, player.Token); 
                     break;
                 }
             }
